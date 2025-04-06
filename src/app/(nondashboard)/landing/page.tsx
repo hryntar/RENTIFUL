dd10a7bde4e6b9890e51f3f9b7c5cadc9e4f6611
hryntar <hrynchuktt@gmail.com>
2025-04-06 18:53:22 +0300

    landing footer implementation

--- src/app/(nondashboard)/landing/page.tsx
+++ src/app/(nondashboard)/landing/page.tsx
@@ -6,6 +6,7 @@ import HeroSection from "./HeroSection";
 import FeaturesSection from "./FeaturesSection";
 import DiscoverSection from "./DiscoverSection";
 import CallToActionSection from "./CallToActionSection";
+import FooterSection from "./FooterSection";
 
 const Landing = () => {
   return (
@@ -14,6 +15,7 @@ const Landing = () => {
       <FeaturesSection />
       <DiscoverSection />
       <CallToActionSection />
+      <FooterSection />
     </div>
   );
 };
